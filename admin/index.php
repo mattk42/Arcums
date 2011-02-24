469f679293537556b219ca876c800d1bdc0cb9af
Matthew Knox <matt1988@gmail.com>
2011-02-24 14:19:00 -0500

    More spring cleaning...

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,6 @@ session_start();
 include("../config.php");
 $_SESSION['currentpage'] = $_SERVER['REQUEST_URI'];
 ?>
-<meta HTTP-EQUIV="REFRESH" content="0; url=<?php echo $root ?>/arcums/playlist/index.php">;
+<meta HTTP-EQUIV="REFRESH" content="0; url=<?php echo $root ?>/admin/playlist/index.php">;
 
 
--- podcast.php
+++ podcast.php
@@ -1,6 +1,6 @@
 <?php
-
-$podcast = simplexml_load_file('http://www.wupx.com/rss/podcast.xml');
+require_once("config.php");
+$podcast = simplexml_load_file($root.'/rss/podcast.xml');
 $item = $podcast->channel->item;
 
 echo "<ul style=\"list-style: none; margin: 5px; padding: 0;\">\n";
@@ -11,10 +11,11 @@ for ($i = 0; $i < 4; $i++) {
 	$item[$i]->enclosure['length'] = substr($item[$i]->enclosure['length'], 0, $length+2);
 	// re-format date string
 	$item[$i]->pubDate = date("F d, Y", strtotime($item[$i]->pubDate)); 
-
-	echo "<li><a href=\"http://www.wupx.com/". $item[$i]->enclosure['url']. "\" title=\"". $item[$i]->description. "\">";
-	echo $item[$i]->title. "</a> -- <span class=\"tinyText\"><i>";
-	echo $item[$i]->pubDate. " -- ". $item[$i]->enclosure['type']. " (". $item[$i]->enclosure['length']. " MB)</i></span></li>";
+	if($item[$i]->title!=''){
+		echo "<li><a href=\"".$root. $item[$i]->enclosure['url']. "\" title=\"". $item[$i]->description. "\">";
+		echo $item[$i]->title. "</a> -- <span class=\"tinyText\"><i>";
+		echo $item[$i]->pubDate. " -- ". $item[$i]->enclosure['type']. " (". $item[$i]->enclosure['length']. " MB)</i></span></li>";
+	}
 }
 echo "</ul>\n";
 
