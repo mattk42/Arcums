2c0b896ae7ef3fdd12e1203ed9b2ffeb7d229f74
mjknox <mjknox@stdout.(none)>
2011-07-27 16:15:15 +0000

    Fixed rowname in preview.php

--- schedule/preview.php
+++ schedule/preview.php
@@ -1,6 +1,6 @@
 <?
 
-$query = "SELECT djid FROM playlist ORDER BY datetime DESC LIMIT 1";
+$query = "SELECT dj_id FROM playlist ORDER BY datetime DESC LIMIT 1";
 $res = mysql_query($query) or die(mysql_error());
 $row = mysql_fetch_array($res);
 $djid = $row[0];
